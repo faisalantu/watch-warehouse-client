7c22824ff11e7455745900405c90a3a01a9f4624
faisalantu <faisalantu@gmail.com>
2022-05-04 04:58:43 +0600

    ⤴️ login register route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
+      <Toaster/>
       <div className='container mx-auto px-2 lg:px-0'>
         <Header />
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
       </div>
       <Footer />
     </div>
