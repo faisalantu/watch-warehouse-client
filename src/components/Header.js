d90ba65e780d6b20833a9a6a337b23f0b8370c12
faisalantu <faisalantu@gmail.com>
2022-05-04 04:57:47 +0600

    🤕 Header responsive done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CustomLink from "./CustomLink";
+import {GiHamburgerMenu} from "react-icons/gi"
+import {signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+  const [user] = useAuthState(auth);
   return (
-    <div className='text-gray-700 '>
+    <nav className='text-gray-700 '>
       <div className='justify-between py-4 hidden md:flex'>
         <div className='font-semibold text-xl flex items-center'>
           <Link to='/'>
@@ -18,9 +24,42 @@ const Header = () => {
           <CustomLink className='mx-1 px-1' to='/about'>
             About
           </CustomLink>
+          {user?
+            <button onClick={()=>{signOut(auth)}} className="mx-1 px-1">logout </button>
+            :
+            <CustomLink className="mx-1 px-1" to='/login'>login</CustomLink>
+            }
         </div>
       </div>
-    </div>
+
+      <div className='p-4 md:hidden'>
+        <div className='flex justify-between'>
+          <div className="font-semibold text-xl flex items-center">
+          <Link to='/'>
+            My Watch
+          </Link>
+          </div>
+            <button onClick={()=>{
+              setOpenMenu(!openMenu)
+            }} className='text-xl'>
+             <GiHamburgerMenu/>  
+            </button>
+        </div>
+          {openMenu?
+          <div className={`flex flex-col mt-4`}>
+            <CustomLink to='/'>Home</CustomLink>
+            <CustomLink to='/about'>
+              About
+          </CustomLink>
+            {user?
+            <div onClick={()=>{signOut(auth)}}>logout </div>
+            :
+            <CustomLink to='/login'>login</CustomLink>
+            }
+          </div>
+          : ""}
+      </div>
+    </nav>
   );
 };
 
