5af4e28914956beba47e4c7c6bf1fc9c47af4690
faisalantu <faisalantu@gmail.com>
2022-05-04 06:08:29 +0600

    👎 Not found page added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import { Route, Routes } from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AuthRequired from "./components/AuthRequired";
+import Inventory from "./pages/Inventory";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div>
-      <Toaster/>
+      <Toaster />
       <div className='container mx-auto px-2 lg:px-0'>
         <Header />
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
+          <Route
+            path='/inventory/:id'
+            element={
+              <AuthRequired>
+                <Inventory />
+              </AuthRequired>
+            }
+          ></Route>
+          <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </div>
       <Footer />
