00ab2170d8d4cf4d41826d6644e00a6cf15ca374
faisalantu <faisalantu@gmail.com>
2022-05-07 02:54:58 +0600

    🐤 axios config added

---
+++ src/axiosConfig.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
