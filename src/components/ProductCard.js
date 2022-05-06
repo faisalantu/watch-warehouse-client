a009e003b40bbcb323f52b3ebf17311f22f4ad9d
faisalantu <faisalantu@gmail.com>
2022-05-07 04:49:16 +0600

    🦫 product card button fixed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import imgg from "../assets/banner.png";
 
 const ProductCard = ({ data }) => {
-  const { title, description, imageUrl, price, quantity, displayName } = data;
+  const { title, description, imageUrl, price, quantity, displayName, _id } = data;
   // const descriptionRender = ()=>{
   //   if(description.lengt)
   // }
@@ -23,7 +23,7 @@ const ProductCard = ({ data }) => {
           <p className='text-xs my-2 text-gray-600'>Seller: {displayName &&displayName}</p>
         </div>
         <Link
-          to={"/inventory/sd"}
+          to={`/inventory/${_id}`}
           className='font-semibold text-gray-800 text-center w-full py-2 bg-green-300 rounded-lg mt-4 block'
         >
           Manage
