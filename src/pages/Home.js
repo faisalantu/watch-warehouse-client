4d0394db5d24a437b10458dc97e1b81e6155dc15
faisalantu <faisalantu@gmail.com>
2022-05-06 23:15:14 +0600

    🍓 HomePage fetch 6 products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import ProductCard from "../components/ProductCard";
 import plant from "../assets/plant.png"
 import clock from "../assets/clock.png"
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchData = async ()=>{
+      const res = await axios.get('http://localhost:5000/api/product/limited')
+      setProducts(res.data)
+    }
+    fetchData()
+  }, [])
+  
   return (
     <div>
       <Banner />
       <div className="flex">
         <div className='flex flex-wrap gap-10 mt-24 lg:w-8/12 justify-center lg:justify-end'>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {products.map((product)=>{
+            return <div key={product._id}> <ProductCard data={product}/></div>
+          })}
         </div>
         <div className="w-4/12 relative hidden lg:block">
           <img style={{right:"20%", top:"20%"}} className="absolute w-52" src={clock} alt="plant" />
