9566c9c4980bc009833bed1f1ccea6081de9ce49
faisalantu <faisalantu@gmail.com>
2022-05-07 04:48:48 +0600

    🐄 manage product card fn added

--- src/components/ManageProductCard.js
+++ src/components/ManageProductCard.js
@@ -1,29 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import imgg from "../assets/banner.png";
 
-const ManageProductCard = () => {
+const ManageProductCard = ({ product,reduceQuantity }) => {
+  const { title, description, imageUrl, price, quantity, displayName } =
+    product;
   return (
     <div className='bg-gray-50 shadow-md rounded-lg w-72 p-3 text-gray-800'>
       <div className=' '>
-        <div className='h-32 bg-white rounded overflow-hidden mb-5'>
-          <img src={imgg} alt='nno0n' />
+        <div className='w-full h-48 bg-white rounded overflow-hidden mb-5'>
+          <img
+            className='object-cover h-full mx-auto'
+            src={imageUrl ? imageUrl : imgg}
+            alt='nno0n'
+          />
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nesciunt
-          sit, ad cum ab possimus id! Tenetur pariatur sapiente vitae.
+        <h1 className='font-semibold my-2 text-lg'>{title && title}</h1>
+        <p className=' overflow-hidden line-clamp-4'>
+          {description && description}
         </p>
         <div>
-          <p className="font-semibold my-2">Price: 100 taka</p>
-          <p className="font-semibold my-2">Quantity: 12</p>
-          <p className="text-xs my-2 text-gray-600">Seller: Faisal Ahmed</p>
+          <p className='font-semibold my-2'>Price: {price && price}$</p>
+          <p className='font-semibold my-2'>Quantity: {quantity && quantity}</p>
+          <p className='text-xs my-2 text-gray-600'>
+            Seller: {displayName && displayName}
+          </p>
         </div>
-        <Link
-          to={"/inventory/sd"}
+        <button
+        onClick={reduceQuantity}
           className='font-semibold text-gray-800 text-center w-full py-2 bg-green-300 rounded-lg mt-4 block'
         >
           Deliver
-        </Link>
+        </button>
       </div>
     </div>
   );
