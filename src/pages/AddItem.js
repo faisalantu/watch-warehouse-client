5f3c3c73c48d4c8486b30cd7d709ef5797d86190
faisalantu <faisalantu@gmail.com>
2022-05-06 23:14:54 +0600

    🍍 Add Item page compleated

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,33 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import auth from "../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
 
 const AddItem = () => {
+  const [user] = useAuthState(auth);
+  const titleRef = useRef("");
+  const imageUrlRef = useRef("");
+  const priceRef = useRef("");
+  const descriptionRef = useRef("");
+  const quantityRef = useRef("");
+  const [fetchingProduct, setFetchingProduct] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    const data = {
+      title: titleRef.current.value,
+      imageUrl: imageUrlRef.current.value,
+      description: descriptionRef.current.value,
+      price: priceRef.current.value,
+      quantity: quantityRef.current.value,
+      userEmail: user?.email,
+      displayName: user?.displayName,
+    };
+    
+    try {
+      setFetchingProduct(true)
+      toast.loading("Creating Product");
+      const res = await axios.post(
+        "http://localhost:5000/api/product",
+         data 
+        
+      );
+      toast.dismiss()
+      setFetchingProduct(false)
+      // console.log(res);
+      toast.success(res?.data?.message)
+    } catch (err) {
+      setFetchingProduct(false)
+      toast.dismiss()
+      toast.error("Something went wrong try again")
+    }
+    
+  };
+
   return (
     <div className='my-32 md:col-span-2'>
       <form
         className='shadow-lg border overflow-hidden rounded-lg lg:w-8/12 mx-auto'
-        action='#'
-        method='POST'
+        onSubmit={handleSubmit}
       >
         <div className='shadow sm:rounded-md sm:overflow-hidden'>
           <div className='px-4 py-5 bg-white space-y-6 sm:p-6'>
             <div>
               <label
-                htmlFor='about'
+                htmlFor='title'
                 className='block text-sm font-medium text-gray-700'
               >
                 Title
               </label>
               <div className='mt-1'>
                 <input
-                  id='about'
-                  name='about'
+                  id='title'
+                  ref={titleRef}
                   className='p-2 shadow-sm outline-none focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md'
                   placeholder='Product Name'
                   defaultValue={""}
+                  required
                 />
               </div>
               <p className='mt-2 text-sm text-gray-500 select-none'>
-                Brief description about the product.
+                Good title for product.
               </p>
             </div>
             <div>
@@ -47,15 +92,17 @@ const AddItem = () => {
                     type='text'
                     className='border px-2 outline-none focus:border-green-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300'
                     placeholder='www.example.com'
+                    ref={imageUrlRef}
+                    required
                   />
                 </div>
               </div>
             </div>
 
-            <div className="flex">
-              <div className="w-6/12 pr-5">
+            <div className='flex'>
+              <div className='w-6/12 pr-5'>
                 <label
-                  htmlFor='about'
+                  htmlFor='quantity'
                   className='block text-sm font-medium text-gray-700'
                 >
                   Quantity
@@ -66,14 +113,15 @@ const AddItem = () => {
                     className='p-2 shadow-sm outline-none focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md'
                     placeholder='0'
                     defaultValue={""}
-                    type="number"
+                    type='number'
+                    ref={quantityRef}
+                    required
                   />
                 </div>
-                
               </div>
-              <div className="w-6/12 lg:pl-5">
+              <div className='w-6/12 lg:pl-5'>
                 <label
-                  htmlFor='about'
+                  htmlFor='price'
                   className='block text-sm font-medium text-gray-700'
                 >
                   Price
@@ -84,7 +132,9 @@ const AddItem = () => {
                     className='p-2 shadow-sm outline-none focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md'
                     placeholder='0'
                     defaultValue={""}
-                    type="number"
+                    type='number'
+                    ref={priceRef}
+                    required
                   />
                 </div>
               </div>
@@ -92,19 +142,20 @@ const AddItem = () => {
 
             <div>
               <label
-                htmlFor='about'
+                htmlFor='description'
                 className='block text-sm font-medium text-gray-700'
               >
                 Description
               </label>
               <div className='mt-1'>
                 <textarea
-                  id='about'
-                  name='about'
+                  id='description'
+                  ref={descriptionRef}
                   rows={3}
                   className='p-2 shadow-sm outline-none focus:ring-green-500 focus:border-green-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md'
                   placeholder='you@example.com'
                   defaultValue={""}
+                  required
                 />
               </div>
               <p className='mt-2 text-sm text-gray-500 select-none'>
@@ -114,8 +165,8 @@ const AddItem = () => {
           </div>
           <div className='px-4 py-3 bg-gray-50 text-right sm:px-6'>
             <button
-              type='submit'
-              className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-green-300 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+              disabled={fetchingProduct}
+              className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-green-300 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2'
             >
               Save
             </button>
