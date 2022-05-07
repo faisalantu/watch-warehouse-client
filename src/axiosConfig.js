6321bfadb3a96fc0136b9838f7aa19c0efb9fb2b
faisalantu <faisalantu@gmail.com>
2022-05-08 04:16:16 +0600

    🧆 axios config modify for jwt

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -7,6 +7,22 @@ const instance = axios.create({
   },
 });
 
+// instance.interceptors.request...
+instance.interceptors.request.use(
+  (request) => {
+    // Edit request config
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      request.headers.common["authHeader"] = token;
+    }
+    //console.log("[request send-]",request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
   (response) => {
