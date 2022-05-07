a19cd6dcb03e3720f192d1439b5b6aeda280ea51
faisalantu <faisalantu@gmail.com>
2022-05-08 04:45:44 +0600

    🚀 RELEASE: changed favicon and axios url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mywatch-faisal.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
