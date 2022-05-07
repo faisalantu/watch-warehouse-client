05b764e4b3b854b2e1d1c83f068bd04851dd1c34
faisalantu <faisalantu@gmail.com>
2022-05-08 04:16:41 +0600

    🐛 BUG: fix

--- src/components/AuthRequired.js
+++ src/components/AuthRequired.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '..//firebase.init';
-
+import auth from '../firebase.init';
+import Loading from './Loading';
 const AuthRequired = ({children}) => {
   const [user, loading] = useAuthState(auth);
   const location = useLocation();
   if(loading){
-      return <div>loading</div>
+      return <Loading/>
   }
   if(!user){
       return <Navigate to="/login" state={{ from: location }} replace />;
